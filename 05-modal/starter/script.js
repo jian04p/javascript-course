1fa6ca79b1f42d364faed886983b2a974e0197e5
Philip Jian Garcia <philipgarcia0409@gmail.com>
2025-09-29 22:42:21 +0800

    feat: implement modal foundation with HTML, CSS, and JS

---
+++ 05-modal/starter/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Selections
+const modalEl = document.querySelector('.modal');
+const overlayEl = document.querySelector('.overlay');
+const btnCloseModalEl = document.querySelector('.close-modal');
+const btnsOpenModalEl = document.querySelectorAll('.show-modal');
+
+console.log('Open buttons:', btnsOpenModalEl.length);
+
+// Helper functions
+const openModal = function () {
+  modalEl.classList.remove('hidden');
+  overlayEl.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modalEl.classList.add('hidden');
+  overlayEl.classList.add('hidden');
+};
+
+// Event listeners
+btnsOpenModalEl.forEach(btn => btn.addEventListener('click', openModal));
+btnCloseModalEl.addEventListener('click', closeModal);
+overlayEl.addEventListener('click', closeModal);
+
+console.log('Modal project ready!');
